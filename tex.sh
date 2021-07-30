828de245cf7ee90c8042c08c2d6966923c77b666
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-07-30 12:33:38 +0200

    code review (todo reduce/remove globals and improve code readability)

--- tex.sh
+++ tex.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # tex.sh:
-#   A script for compiling latex documents.
+#   Helper script for compiling latex documents.
 #
-#   This script, when bound to a global alias
+#   When bound to a global alias, the script
 #   makes it easy to compile a latex project
 #   from any child directory of the project.
 #
@@ -11,135 +11,120 @@
 # Created : 2020-10-19 06:59
 # Github  : https://github.com/antiqueeverett/
 
-# print with less no line breaks
-export max_print_line=1000
+# -- remove line breaks from compilation output
 export error_line=254
 export half_error_line=238
+export max_print_line=1000
 
-# clean
-#   Cleans residual log files.
-clean() {
-    # use " ./ " so names with dashes won't become options
-    rm -rf ./*.aux ./*.log ./*.bbl ./*.out ./*.toc ./*.gz ./*.lof ./*.lot ./*.cut ./*.blg ./*.nav ./*.snm ./*.bcf ./*.xml ./*.upa ./*.upb ./*.equ ./*.alg ./*.exc ./*.rep
-}
+# -- share variables
+main="main.tex"   # name of main latex project file
+draft="draft.tex" # name of secondary latex project file
+location=""       # path to main/draft latex project file
+textonly=""       # flag to build draft
+pdf=""            # compiled pdf file
 
-compileSketch() {
-    printf "\n(1/4): -draftmode -halt-on-error -file-line-error\n"
-    pdflatex -draftmode -interaction=nonstopmode "check.tex" | grep 'error\|critical\|Error\|Critical' | grep -v "(/"
-
-    printf "\n(2/4): bibtex\n"
-    if [ -f "check.aux" ]; then bibtex "check.aux"; fi | grep 'warning\|error\|critical\|Warning\|Error\|Critical'
-        bibtex bu
-        bibtex bu1
-
-        printf "\n(3/4): -draftmode -halt-on-error -file-line-error"
-        pdflatex -draftmode -interaction=nonstopmode "check.tex" | grep 'error\|critical\|Error\|Critical' | grep -v "(/"
-
-        printf "\n(4/4): -interaction=nonstopmode\n"
-        pdflatex -interaction=nonstopmode "check.tex" >/dev/null 2>&1
-
-        pdftotext check.pdf check.txt
-        echo `tr '\n' ' ' < check.txt` > check.txt
-        #rm -rf check.pdf
-    }
-
-# compile
-#   Compiles latex project.
-#
-# ARGUMENTS:
-#   $1 name of main '*.tex' file
-compile() {
-
-    if [ -f "$pdf" ]; then rm -f "$pdf"; fi
-
-    pdf=${1/tex/pdf} # output '*.pdf' file
-    aux=${1/tex/aux} # output '*.aux' file
-
-    printf "\n(1/4): -draftmode -halt-on-error -file-line-error\n"
-    pdflatex -draftmode -interaction=nonstopmode "$1" | grep 'error\|critical\|Error\|Critical' | grep -v "(/"
-
-    printf "\n(2/4): bibtex\n"
-    if [ -f "$aux" ]; then bibtex "$aux"; fi | grep 'warning\|error\|critical\|Warning\|Error\|Critical'
-        bibtex bu
-        bibtex bu1
-
-        printf "\n(3/4): -draftmode -halt-on-error -file-line-error"
-        pdflatex -draftmode -interaction=nonstopmode "$1" | grep 'error\|critical\|Error\|Critical' | grep -v "(/"
-
-        printf "\n(4/4): -interaction=nonstopmode\n"
-        pdflatex -interaction=nonstopmode "$1" >/dev/null 2>&1
-
-        compileSketch
-        clean
-    }
-
-# show
-#   Opens '*.pdf' file.
+# -- show compiled pdf
+#    args: $1 main latex project file
 show() {
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        gio open main.pdf
-        #gio open check.pdf
+    if [ -f "$pdf" ]; then
+        if [[ "$OSTYPE" == "linux-gnu" ]]; then
+            gio open "$pdf"
+        else
+            [[ "$OSTYPE" == "darwin" ]]
+            open "$pdf"
+        fi
     else
-        [[ "$OSTYPE" == "darwin" ]]
-        open main.pdf
-        #open check.pdf
+        echo "-- couldn't find pdf file"
     fi
 }
 
-# findMain:
-#   Finds main latex file.
-#
-# ARGUMENTS:
-#   $1 name of main latex file
-findMain() {
-    topmost=10
-    FOUND=
+# -- remove pdflatex compilation cache files
+clean() {
+    rm -rf ./*.aux ./*.log ./*.bbl ./*.out ./*.toc ./*.gz ./*.lof ./*.lot ./*.cut ./*.blg ./*.nav ./*.snm ./*.bcf ./*.xml ./*.upa ./*.upb ./*.equ ./*.alg ./*.exc ./*.rep
+}
 
-    #   traverse parent directories upward to find main.tex
+# -- check for bib units
+bibUnits() {
+    if compgen -G "bu*" >/dev/null; then
+        bibtex bu
+        bibtex bu1
+    fi
+}
+
+# -- compile pdf
+#    args: $1 main latex project file
+compile() {
+    file=${1/tex/pdf} # output '*.pdf' file
+    aux=${1/tex/aux}  # output '*.aux' file
+
+    if [ -f "$file" ]; then rm -f "$file"; fi
+
+    printf "\n(1/4) compilation args: -draftmode -halt-on-error -file-line-error"
+    pdflatex -draftmode -interaction=nonstopmode "$1" | grep 'error\|critical\|Error\|Critical' | grep -v "(/"
+
+    printf "\n(2/4) compiling bibliographies"
+    if [ -f "$aux" ]; then bibtex "$aux"; fi | grep 'warning\|error\|critical\|Warning\|Error\|Critical' | grep -v "(There was 1 error message)"
+    bibUnits
+
+    printf "\n(3/4) compilation args: -draftmode -halt-on-error -file-line-error"
+    pdflatex -draftmode -interaction=nonstopmode "$1" | grep 'error\|critical\|Error\|Critical' | grep -v "(/"
+
+    printf "\n(4/4) compilation args: -interaction=nonstopmode\n\n"
+    pdflatex -interaction=nonstopmode "$1" >/dev/null 2>&1
+
+    pdf="$file"
+}
+
+# -- compile text only pdf
+#    args: $1 main latex project file
+compileTextonly() {
+    file=${1/tex/pdf} # output '*.pdf' file
+    aux=${1/tex/aux}  # output '*.aux' file
+
+    if [ -f "$file" ]; then rm -f "$file"; fi
+
+    printf "\n(1/4) compilation args: -draftmode -halt-on-error -file-line-error"
+    pdflatex -draftmode -interaction=nonstopmode "$1" | grep 'error\|critical\|Error\|Critical' | grep -v "(/"
+
+    printf "\n(2/4) compiling bibliographies"
+    if [ -f "$aux" ]; then bibtex "$aux"; fi | grep 'warning\|error\|critical\|Warning\|Error\|Critical' | grep -v "(There was 1 error message)"
+    bibUnits
+
+    printf "\n(3/4) compilation args: -draftmode -halt-on-error -file-line-error"
+    pdflatex -draftmode -interaction=nonstopmode "$1" | grep 'error\|critical\|Error\|Critical' | grep -v "(/"
+
+    printf "\n(4/4) compilation args: -interaction=nonstopmode\n\n"
+    pdflatex -draftmode -interaction=nonstopmode "$1" >/dev/null 2>&1
+
+    pdf="$file"
+
+    pdftotext draft.pdf draft.txt
+    echo $(tr '\n' ' ' <draft.txt) >draft.txt
+}
+
+# -- find main project file
+#    args: $1 main project file
+autofind() {
+    topmost=10
+
+    # traverse parent directories upward and find main latex project file
     for ((maxwalk = topmost; maxwalk > 0; --maxwalk)); do
         for file in *.tex; do
-
             if [ "$file" = "$main" ]; then
-
-                # grep -w
-                #   select only those lines containing
-                #   matches that form whole words
-                #
-                # grep -q (tip: remove for trouble shooting)
-                #   do not write anything to standard output & exit
-                #   immediately with zero status match is found
-                #
                 if grep -w -q 'documentclass' "$PWD/$file"; then
-                    FOUND='yes'
-                    echo "Found $main"
-
-                    # compile latex project
-                    compile "$file"
-
-                    # open '*.pdf' file iff it exists
-                    if [ -f "$pdf" ]; then show; fi
-
-                    return # break loop if we find what we are looking for
+                    location=$file
+                    true
+                    return
                 fi
             fi
         done
         cd "../"
     done
-    if [ "$FOUND" = '' ]; then echo "$main not found"; fi
+
+    false
 }
 
-# setMainFile
-#   Sets main '*.tex' project file to be searched for.
-setMainFile() {
-    echo "Searching for $main"
-    main="$1"
-    findMain "$main"
-}
-
-# isTextFile
-#   Checks for a '*.tex' file argument.
-# @retval
-#   return true if latex file argument found.
+# -- verify file type
 isTexFile() {
     fileExtension='.tex'
     if echo "$1" | grep -q "$fileExtension"; then
@@ -149,30 +134,77 @@ isTexFile() {
     fi
 }
 
-# checkargs
-#   Safeguards arguments (naively) and
-#     prints help execution directives.
+# -- show usage
+showUsage() {
+    printf "\n-- invalid arguments specified"
+    printf "\n-- example usage:>_ tex.sh myproject.tex\n\n"
+}
+
+# -- evaluate arguments
 checkargs() {
-    if (($# > 2)); then
-        echo "Invalid argument"
-        echo "example:"
-        echo "       >_ tex.sh main.tex"
-        echo "       >_ tex.sh main.tex -j10"
+    echo "$@"
+    if (($# > 3)); then
+        showUsage
+        false
     else
         if isTexFile "$1"; then
-            setMainFile "$1"
-        elif isTexFile "$2"; then
-            setMainFile "$2"
+            main="$1"
+
+            for i in "$@"; do
+                if [ "$i" = "-draft" ]; then
+                    textonly="Yes"
+                fi
+            done
+        else
+            false
+            showUsage
         fi
+        true
     fi
 }
 
-# default behaviour sets 'main.tex'
-#   as latex main project file
-main="main.tex"
+build() {
+    if autofind "$main"; then
+        echo "-- found $main"
+        compile "$location"
+        clean
+
+        if [ -f "$pdf" ]; then
+            show "$pdf"
+        else
+            echo "-- couldn't find pdf file"
+        fi
+
+    else
+        echo "-- couldn't find $main file"
+    fi
+}
+
+buildTextonly() {
+    if autofind "$draft"; then
+        echo "-- found $draft"
+        compileTextonly "$location"
+        clean
+        show
+    else
+        echo "-- couldn't find $main file"
+    fi
+}
+
+# -- autofind main latex project file
 if [ $# -eq 0 ]; then
-    echo "Searching for $main" # Iff no args are specified
-    findMain "$main"           # ... find 'main.tex'
+    printf "\n-- no arguments were specified\n"
+    printf "\n-- searching for %s\n" "$main"
+    build
+
 else
-    checkargs "$@"
+    if checkargs "$@"; then
+        printf "\n-- searching for %s\n" "$main"
+        build
+        if [ "$textonly" = "Yes" ]; then
+            echo "-- compiling 'textonly' draft"
+            main="$draft"
+            buildTextonly
+        fi
+    fi
 fi
